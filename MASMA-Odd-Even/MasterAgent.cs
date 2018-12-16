ac2dd797b4e162f6e3a211fa4dc0887c00c2d9da
gcrismaruc <george.crismaruc@gmail.com>
2018-12-16 17:49:21 +0200

    Implement odd-even algorithm
    Convert enum to string for comparison in Comparator agent.

---
+++ MASMA-Odd-Even/MasterAgent.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActressMas;
+
+
+using MASMA_proiect.agents;
+
+namespace MASMA_Odd_Even
+{
+    class MasterAgent : ActressMas.Agent
+    {
+
+        int NumberOfPhases;
+        int[] array;
+        public List<string> comparatorAgents;
+        int currentPhase = 1;
+        int numberOfReceivedMessages = 0;
+        bool isNumberOfElementsEven;
+
+        public MasterAgent(int numberOfPhases, int[] array, List<string> agents, bool isNumberOfElementsEven)
+        {
+            this.NumberOfPhases = numberOfPhases;
+            this.array = array;
+            this.comparatorAgents = agents;
+            this.isNumberOfElementsEven = isNumberOfElementsEven;
+        }
+
+        public override void Setup()
+        {
+            Console.WriteLine("Array sent to comparator agents!");
+            sendArrayToAgentDueToThePhase(currentPhase, isNumberOfElementsEven);
+        }
+
+        public override void Act(Message message)
+        {
+            string agentName = message.Sender;
+            int index = Int32.Parse(agentName.Replace("comparatorAgent", ""));
+
+            //Console.WriteLine("Message received from :" + agentName + " calculated index = " + index);
+
+            numberOfReceivedMessages++;
+
+            string[] splittedMessage = message.Content.Split('#');
+            string action = splittedMessage[0];
+            string[] parameters = splittedMessage[1].Split(',');
+
+            if (isNumberOfElementsEven)
+            {
+                if (currentPhase % 2 != 0)
+                {
+                    array[2 * index] = Int32.Parse(parameters[0]);
+                    array[2 * index + 1] = Int32.Parse(parameters[1]);
+
+                    if (numberOfReceivedMessages == array.Length / 2)
+                    {
+                        sendAndUpdateCurrentPhase();
+                        numberOfReceivedMessages = 0;
+                    }
+                }
+                else
+                {
+                    array[2 * index + 1] = Int32.Parse(parameters[0]);
+                    array[2 * index + 2] = Int32.Parse(parameters[1]);
+
+                    if (numberOfReceivedMessages == array.Length / 2 - 1)
+                    {
+                        sendAndUpdateCurrentPhase();
+                        numberOfReceivedMessages = 0;
+                    }
+                }
+            }
+            else
+            {
+                if (currentPhase % 2 != 0)
+                {
+                    array[2 * index] = Int32.Parse(parameters[0]);
+                    array[2 * index + 1] = Int32.Parse(parameters[1]);
+
+                    if (numberOfReceivedMessages == array.Length / 2)
+                    {
+                        sendAndUpdateCurrentPhase();
+                        numberOfReceivedMessages = 0;
+                    }
+                }
+                else
+                {
+                    array[2 * index + 1] = Int32.Parse(parameters[0]);
+                    array[2 * index + 2] = Int32.Parse(parameters[1]);
+
+                    if (numberOfReceivedMessages == array.Length / 2)
+                    {
+                        sendAndUpdateCurrentPhase();
+                        numberOfReceivedMessages = 0;
+                    }
+                }
+            }
+        }
+
+        void sendAndUpdateCurrentPhase()
+        {
+            currentPhase++;
+
+            Console.WriteLine("Current phase: " + currentPhase);
+            Console.WriteLine(string.Join(",", array));
+            if (currentPhase <= NumberOfPhases)
+            {
+                sendArrayToAgentDueToThePhase(currentPhase, isNumberOfElementsEven);
+            }
+
+            numberOfReceivedMessages = 0;
+        }
+
+        void sendArrayToAgentDueToThePhase(int phase, bool isEven)
+        {
+            if (isEven)
+            {
+                if (phase % 2 != 0)
+                {
+                    sendForOddPhase(0);
+                }
+                else
+                {
+                    sendForEvenPhase(1);
+                }
+            }
+            else
+            {
+                if (phase % 2 != 0)
+                {
+                    sendForOddPhase(0);
+                }
+                else
+                {
+                    sendForEvenPhase(0);
+                }
+            }
+        }
+
+        void sendForOddPhase(int offset)
+        {
+            for (int i = 0; i < comparatorAgents.Count - offset; i++)
+            {
+                string message = MASMA_proiect.agents.Actions.COMPARE + "#" + array[2 * i] + "," + array[(2 * i) + 1];
+                Console.WriteLine("Message: " + message + " was sent to : " + comparatorAgents.ElementAt(i));
+                this.Send(comparatorAgents.ElementAt(i), message);
+            }
+        }
+
+        void sendForEvenPhase(int offset)
+        {
+            for (int i = 0; i < comparatorAgents.Count - offset; i++)
+            {
+                string message = MASMA_proiect.agents.Actions.COMPARE + "#" + array[2 * i + 1] + "," + array[2 * i + 2];
+                Console.WriteLine("Message: " + message + " was sent to : " + comparatorAgents.ElementAt(i));
+                this.Send(comparatorAgents.ElementAt(i), message);
+            }
+        }
+    }
+}
--- MASMA-Odd-Even/Program.cs
+++ MASMA-Odd-Even/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MASMA_proiect.agents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,15 +9,50 @@ namespace MASMA_Odd_Even
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int[] array = { 2, 1, 8, 5, -32, -2321, 2,2,12,12,33,12, 321, 3, -21321};
+            int numberOfPhases;
+            int numberOfAgents;
+            bool isNumberOfElementsEven;
+
+            if (array.Length % 2 == 0)
+            {
+                isNumberOfElementsEven = true;
+            }
+            else
+            {
+                isNumberOfElementsEven = false;
+            }
+
+            numberOfPhases = array.Length;
+            numberOfAgents = array.Length / 2;
+
+            var env = new ActressMas.Environment();
+
+            List<string> comparatorsList = new List<string>();
+            for (int i = 0; i < numberOfAgents; i++)
+            {
+                ComparatorAgent ComparatorAgent = new MASMA_proiect.agents.ComparatorAgent();
+                string agentName = "comparatorAgent" + i;
+                env.Add(ComparatorAgent, agentName);
+
+                comparatorsList.Add(agentName);
+                ComparatorAgent.Start();
+            }
+
+            Console.WriteLine("Number of comparator agents = " + comparatorsList.Count);
+            Console.WriteLine("Number of phases = " + numberOfPhases);
+            comparatorsList.ForEach(ag => Console.Write(ag));
+
+
+            MasterAgent MasterAgent = new MASMA_Odd_Even.MasterAgent(numberOfPhases, array, comparatorsList, isNumberOfElementsEven);
+            
+            env.Add(MasterAgent, "masterAgent");
+            MasterAgent.Start();
+
+            Console.WriteLine("Main started");
+            env.WaitAll();
         }
     }
 }
